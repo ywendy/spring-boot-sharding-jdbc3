7ec6a5973d236fbe1321651449cbcb33824cc94c
lee <lisubin@didiglobal.com>
2019-06-25 21:30:53 +0800

    add

--- spring-boot-sharding-common3/src/test/java/com/abin/lee/sharding/jdbc/common/test/AppCommonTest.java
+++ spring-boot-sharding-common3/src/test/java/com/abin/lee/sharding/jdbc/common/test/AppCommonTest.java
@@ -4,4 +4,8 @@ package com.abin.lee.sharding.jdbc.common.test;
  * Created by abin on 2018/6/6.
  */
 public class AppCommonTest {
+    public static void main(String[] args) {
+        long time = System.currentTimeMillis();
+        System.out.println(time);
+    }
 }
